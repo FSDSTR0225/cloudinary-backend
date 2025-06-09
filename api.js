23c3add660c4cb43d342571a8b466b643afc22b6
Jordi Galobart Salvat <jgalobart@gmail.com>
2025-06-09 11:38:25 +0200

    created endpoint

---
+++ api.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = 3003
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.post('/profile/edit', (req, res) => {
+    console.log("form received")
+    res.send({"msg":"received"})
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
