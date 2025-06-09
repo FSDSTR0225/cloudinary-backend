2cc3eed59786dc5d8a6f8bab70b1a7ee14cd7713
Jordi Galobart Salvat <jgalobart@gmail.com>
2025-06-09 12:43:31 +0200

    solved

--- api.js
+++ api.js
@@ -1,21 +1,37 @@
 const express = require('express')
 const cors = require('cors')
 const multer  = require('multer')
+const cloudinary = require('cloudinary').v2
 const app = express()
 const port = 3003
+
+require('dotenv').config()
 
 app.use(express.json());
 app.use(cors())
 
-const upload = multer({ dest: 'uploads/' })
+const upload = multer({ storage: multer.memoryStorage() })
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.post('/profile/edit', upload.any(), (req, res) => {
+app.post('/profile/edit', upload.any(), async (req, res) => {
     console.log("form received", req.body)
     console.log("files received", req.files)
+
+    for (const file of req.files) {
+        const base64 = file.buffer.toString('base64')
+        const dataURI = `data:${file.mimetype};base64,${base64}`
+        const result = await cloudinary.uploader.upload(dataURI);
+    }
+
     res.send({"msg":"received"})
 })
 
